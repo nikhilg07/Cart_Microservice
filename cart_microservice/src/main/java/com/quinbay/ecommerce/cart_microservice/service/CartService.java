94cd9a06efc4afb820b16c56193bae8aa015f8b0
nikhilg07 <43913706+nikhilg07@users.noreply.github.com>
2022-03-23 10:08:38 +0530

    Update CartService.java

--- cart_microservice/src/main/java/com/quinbay/ecommerce/cart_microservice/service/CartService.java
+++ cart_microservice/src/main/java/com/quinbay/ecommerce/cart_microservice/service/CartService.java
@@ -2,9 +2,9 @@ package com.quinbay.ecommerce.cart_microservice.service;
 
 public interface CartService {
 
-    void addProduct(Long cartId,Long userId,Long productId);
+    void addProduct(Long cartId,Long userId,Long productId,Long sellerId);
 
-    void deleteProduct(Long userId,Long productId);
+    void deleteProduct(Long userId,Long productId,Long sellerId);
 
 
 }
